d18b3b66149df414dbaaa1a5c2a96a31c178a8f1
danditk <danditkaczuk@gmail.com>
2020-05-18 17:04:38 +0200

    ALL excercises from MySQL_2 done

--- MySQL_2/kurs_mysql2/zadanie domowe/samochody/dbconnect.php
+++ MySQL_2/kurs_mysql2/zadanie domowe/samochody/dbconnect.php
@@ -2,5 +2,5 @@
 	$host="localhost"; // Nazwa hosta
 	$user="root"; // Nazwa uzytkownika mysql
 	$password=""; // Haslo do bazy
-	$database="samochody"; // Nazwa bazy
+	$database="wypozyczalnia_samochodow"; // Nazwa bazy
 ?>
